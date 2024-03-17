5799d6951197770e403998f6b9afa69c40e70fce
haryprasadh <hariprasad81026@gmail.com>
2024-03-17 18:55:48 +0530

    first commit

--- index.js
+++ index.js
@@ -1,35 +1,75 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const OMDBapikey = "352838b6";
-const OMDBurl = `https://www.omdbapi.com/?apikey=${OMDBapikey}`;
+const mainDiv = document.getElementById("mainDiv");
 
 async function main() {
-  let html = await fetch("https://tube.ibomma.pw/telugu-movies/").then((res) =>
-    res.text()
-  );
+  mainDiv.innerHTML = "";
+  const load = document.createElement("img");
+  load.src = "./loading.gif";
+  mainDiv.append(load);
 
-  let dom = new JSDOM(html);
+  const targetUrl = "https://tube.ibomma.pw/telugu-movies/";
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const OMDBapikey = "352838b6";
+  const OMDBurl = `https://www.omdbapi.com/?apikey=${OMDBapikey}`;
 
-  let movies = dom.window.document.querySelectorAll(".entry-title");
-  for (let movie of movies) {
-    let name = movie.textContent.substring(0, movie.textContent.length - 4);
-    let year = movie.textContent.substring(movie.textContent.length - 4);
-    let link = movie.querySelector("a").href;
+  try {
+    const response = await fetch(proxyUrl + targetUrl);
+    const html = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
 
-    let fullURL = OMDBurl + `&t=${name}&y=${year}`;
-    let movieData = await fetch(fullURL).then((res) => res.json());
-    if (movieData.Response == "False") continue;
+    const movies = doc.querySelectorAll(".entry-title");
+    mainDiv.innerHTML = "";
+    for (let movie of movies) {
+      let name = movie.textContent.substring(0, movie.textContent.length - 4);
+      let year = movie.textContent.substring(movie.textContent.length - 4);
+      let link = movie.querySelector("a").href;
 
-    console.log(movieData.Title);
-    // console.log(movieData.Poster);
-    console.log(movieData.Plot);
-    console.log(movieData.imdbRating);
-    console.log(movieData.Genre);
-    console.log(movieData.Runtime);
-    console.log(movieData.Type);
-    console.log(link);
-    console.log("////////////////////////////////////");
+      let fullURL = OMDBurl + `&t=${name}&y=${year}`;
+      const movieDataResponse = await fetch(fullURL);
+      const movieData = await movieDataResponse.json();
+
+      if (movieData.Response == "False") continue;
+      let rating = movieData.imdbRating;
+      if (rating < 8 || rating === "N/A") continue;
+
+      const div = document.createElement("div");
+
+      const h3 = document.createElement("h3");
+      h3.textContent = `Movie title: ${movieData.Title}`;
+      h3.setAttribute("style", "width:20vw;color:darkred");
+
+      const img = document.createElement("img");
+      img.src = movieData.Poster;
+      img.setAttribute("style", "width:10vw");
+
+      const p = document.createElement("p");
+      p.textContent = `PLOT: ${movieData.Plot}`;
+      p.setAttribute("style", "width:20vw");
+
+      const h41 = document.createElement("h4");
+      h41.textContent = `IMDB rating: ${movieData.imdbRating}`;
+      h41.setAttribute("style", "width:20vw");
+
+      const h42 = document.createElement("h4");
+      h42.textContent = `Genre: ${movieData.Genre}`;
+      h42.setAttribute("style", "width:20vw");
+
+      const h43 = document.createElement("h4");
+      h43.textContent = `Runtime: ${movieData.Runtime}`;
+      h43.setAttribute("style", "width:20vw");
+
+      const h44 = document.createElement("h4");
+      h44.textContent = `Type: ${movieData.Type}`;
+      h44.setAttribute("style", "width:20vw");
+
+      const a = document.createElement("a");
+      a.textContent = `open ${movieData.Title}`;
+      a.href = link;
+
+      div.append(h3, img, h41, h42, h44, h43, p, a);
+      mainDiv.append(div);
+    }
+  } catch (error) {
+    console.error("Error:", error);
   }
 }
-
-main();
