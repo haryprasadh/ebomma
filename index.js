c975afac3cc1d3209e984792eb3939c9b160841d
haryprasadh <hariprasad81026@gmail.com>
2024-03-18 20:07:00 +0530

    added mediaqueries

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+const targetUrl = "https://tube.ibomma.pw/telugu-movies/";
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const OMDBapikey = "352838b6";
+const OMDBurl = `https://www.omdbapi.com/?apikey=${OMDBapikey}`;
+
 const mainDiv = document.getElementById("mainDiv");
 
 async function main() {
   mainDiv.innerHTML = "";
   const load = document.createElement("img");
-  load.src = "./loading.gif";
+  load.src = "./images/loading.gif";
   mainDiv.append(load);
-
-  const targetUrl = "https://tube.ibomma.pw/telugu-movies/";
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const OMDBapikey = "352838b6";
-  const OMDBurl = `https://www.omdbapi.com/?apikey=${OMDBapikey}`;
 
   try {
     const response = await fetch(proxyUrl + targetUrl);
@@ -36,37 +36,41 @@ async function main() {
 
       const h3 = document.createElement("h3");
       h3.textContent = `Movie title: ${movieData.Title}`;
-      h3.setAttribute("style", "width:20vw;color:darkred");
+      h3.setAttribute("style", "width:90%;color:darkred");
 
       const img = document.createElement("img");
       img.src = movieData.Poster;
-      img.setAttribute("style", "width:10vw;border-radius:3%");
+      img.setAttribute("style", "width:50%;border-radius:3%");
 
       const p = document.createElement("p");
       p.textContent = `PLOT: ${movieData.Plot}`;
-      p.setAttribute("style", "width:20vw");
+      p.setAttribute("style", "width:90%");
 
       const h41 = document.createElement("h4");
       h41.textContent = `IMDB rating: ${movieData.imdbRating}`;
-      h41.setAttribute("style", "width:20vw");
+      h41.setAttribute("style", "width:90%");
 
       const h42 = document.createElement("h4");
       h42.textContent = `Genre: ${movieData.Genre}`;
-      h42.setAttribute("style", "width:20vw");
+      h42.setAttribute("style", "width:90%");
 
       const h43 = document.createElement("h4");
       h43.textContent = `Runtime: ${movieData.Runtime}`;
-      h43.setAttribute("style", "width:20vw");
+      h43.setAttribute("style", "width:90%");
 
       const h44 = document.createElement("h4");
       h44.textContent = `Type: ${movieData.Type}`;
-      h44.setAttribute("style", "width:20vw");
+      h44.setAttribute("style", "width:90%");
 
       const a = document.createElement("a");
       a.textContent = `open ${movieData.Title}`;
       a.href = link;
+      a.setAttribute("style", "width:90%");
 
       div.append(h3, img, h41, h42, h44, h43, p, a);
+      div.classList.add("small-div");
+      div.setAttribute("style", "background-color:rgb(76, 115, 87)");
+
       mainDiv.append(div);
     }
   } catch (error) {
