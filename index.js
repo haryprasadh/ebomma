6ad283a46a1f955e06b9a9f5fc674bb060bd365a
haryprasadh <hariprasad81026@gmail.com>
2024-03-19 06:36:57 +0530

    added some styling

--- index.js
+++ index.js
@@ -43,31 +43,54 @@ async function main() {
       const div = document.createElement("div");
 
       const h3 = document.createElement("h3");
-      h3.textContent = `Movie title: ${movieData.Title}`;
-      h3.setAttribute("style", "width:90%;color:darkred");
+      const span1 = document.createElement("span");
+      span1.textContent = `${movieData.Title}`;
+      span1.className = "highlight";
+      h3.appendChild(span1);
+      h3.setAttribute("style", "width:90%;");
 
       const img = document.createElement("img");
       img.src = movieData.Poster;
       img.setAttribute("style", "width:50%;border-radius:3%");
 
-      const p = document.createElement("p");
-      p.textContent = `PLOT: ${movieData.Plot}`;
-      p.setAttribute("style", "width:90%");
+      const h5 = document.createElement("h5");
+      const span2 = document.createElement("span");
+      span2.textContent = "PLOT: ";
+      span2.className = "highlight2";
+      h5.appendChild(span2);
+      h5.appendChild(document.createTextNode(movieData.Plot));
+      h5.setAttribute("style", "width:90%");
 
       const h41 = document.createElement("h4");
-      h41.textContent = `IMDB rating: ${movieData.imdbRating}`;
+      const span3 = document.createElement("span");
+      span3.textContent = "IMDB rating: ";
+      span3.className = "highlight2";
+      h41.appendChild(span3);
+      h41.appendChild(document.createTextNode(movieData.imdbRating));
       h41.setAttribute("style", "width:90%");
 
       const h42 = document.createElement("h4");
-      h42.textContent = `Genre: ${movieData.Genre}`;
+      const span4 = document.createElement("span");
+      span4.textContent = "Genre: ";
+      span4.className = "highlight2";
+      h42.appendChild(span4);
+      h42.appendChild(document.createTextNode(movieData.Genre));
       h42.setAttribute("style", "width:90%");
 
       const h43 = document.createElement("h4");
-      h43.textContent = `Runtime: ${movieData.Runtime}`;
+      const span5 = document.createElement("span");
+      span5.textContent = "Runtime: ";
+      span5.className = "highlight2";
+      h43.appendChild(span5);
+      h43.appendChild(document.createTextNode(movieData.Runtime));
       h43.setAttribute("style", "width:90%");
 
       const h44 = document.createElement("h4");
-      h44.textContent = `Type: ${movieData.Type}`;
+      const span6 = document.createElement("span");
+      span6.textContent = "Type: ";
+      span6.className = "highlight2";
+      h44.appendChild(span6);
+      h44.appendChild(document.createTextNode(movieData.Type));
       h44.setAttribute("style", "width:90%");
 
       const a = document.createElement("a");
@@ -75,7 +98,7 @@ async function main() {
       a.href = link;
       a.setAttribute("style", "width:90%");
 
-      div.append(h3, img, h41, h42, h44, h43, p, a);
+      div.append(h3, img, h41, h42, h44, h43, h5, a);
       div.classList.add("small-div");
       div.setAttribute("style", "background-color:rgb(120, 160, 131)");
 
